b96f2320144cd2435afb33184c0568dfb6abefa7
LivinkumarS <001liv100@ggmail.com>
2025-01-10 13:30:06 +0530

    components and RFC. Import and Export and styling in React App

---
+++ src/components/Name.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+export default function Name() {
+  return (
+    <div>
+        
+        <h1 className='head'>Name: Nivash</h1>
+        <h1>Age: 25</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium reprehenderit, expedita in ullam provident quisquam! Iste, error. Quia doloremque, tempora minima non rem veritatis libero magnam hic nam aut mollitia!</p>
+        <h2>kanna</h2>
+
+    </div>
+  )
+}
