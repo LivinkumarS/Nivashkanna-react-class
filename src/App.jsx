7eb0683431371eaa0d0f4efd26da5c5b75ab20db
LivinkumarS <001liv100@ggmail.com>
2025-01-18 13:14:25 +0530

    Hooks-UseEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 export default function App() {
+  const nameList = ["Ajith", "Vijay", "Surya", "Vikram", "Dhanush"];
+
   const [number, setNumber] = useState(0);
+
+
+  useEffect(() => {
+    console.log("Changed Name");
+  }, [number]);
 
   return (
     <div>
-      <h1>{number}</h1>
+      <h1>{nameList[number]}</h1>
       <button
         onClick={() => {
           setNumber((prev) => {
+            if (prev == 4) {
+              return 0;
+            }
             return ++prev;
           });
         }}
