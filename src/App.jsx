f1dc53f4cf4232ecfbf14792b0fa2454793c5d1a
LivinkumarS <001liv100@ggmail.com>
2025-01-20 13:54:22 +0530

    Hooks---> UseRef(2)

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,18 @@
 import React from "react";
 import { useRef } from "react";
-import { useState } from "react";
-import { useEffect } from "react";
 
 export default function App() {
-  const [number, setNumber] = useState(0);
-
-  const refNumber = useRef(0);
-
-
-  useEffect(() => {
-    console.log("Rendered");
-  });
+  const inputRef = useRef();
 
   return (
     <div>
+      <input ref={inputRef} type="text" />
       <button
         onClick={() => {
-          setNumber((prev) => {
-            return ++prev;
-          });
+          inputRef.current.focus();
         }}
       >
-        useState inc
-      </button>
-
-      <button
-        onClick={() => {
-          refNumber.current++;
-          console.log(refNumber.current);
-        }}
-      >
-        useRef inc
+        Click me
       </button>
     </div>
   );
