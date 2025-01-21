3bb444acb5bc52ca866341ad9a6e9a376071eebe
LivinkumarS <001liv100@ggmail.com>
2025-01-21 12:55:42 +0530

    Hooks--> useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,52 @@
 import React from "react";
-import { useRef } from "react";
+import { useReducer } from "react";
+import { useState } from "react";
+
+function reducerFn(state, action) {
+  if (action == "increment") {
+    return { key: state.key + 1 };
+  } else if (action === "decrement") {
+    return { key: state.key - 1 };
+  } else {
+    return { key: state.key * 2 };
+  }
+}
 
 export default function App() {
-  const inputRef = useRef();
+  // const [count, setCount] = useState(0);
+
+  const [count, dispatch] = useReducer(reducerFn, { key: 0 });
 
   return (
-    <div>
-      <input ref={inputRef} type="text" />
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "10px",
+      }}
+    >
       <button
         onClick={() => {
-          inputRef.current.focus();
+          dispatch("increment");
         }}
       >
-        Click me
+        inc
+      </button>
+      <h1>{count.key}</h1>
+      <button
+        onClick={() => {
+          dispatch("decrement");
+        }}
+      >
+        dec
+      </button>
+      <button
+        onClick={() => {
+          dispatch("double");
+        }}
+      >
+        double
       </button>
     </div>
   );
