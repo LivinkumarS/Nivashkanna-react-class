2cb6a8fbc0dc7893fd3bbc6db72137ed8d0781cc
LivinkumarS <001liv100@ggmail.com>
2025-01-22 13:20:04 +0530

    Controlled Inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,13 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from "react";
 
 export default function App() {
-  const output = [
-    {
-      title: "Fav Foods",
-      elements: ["Biriyani", "Meals", "Dosa", "Idly", "Uthappam"],
-    },
-    {
-      title: "Fav Snacks",
-      elements: ["Laddu", "Mixture", "Biscuits", "Halwa"],
-    },
-    {
-      title: "Fav Movies",
-      elements: ["Thuppakki", "96", "Alai Payuthey", "GOAT"],
-    },
-  ];
+  const [inpValue, setInpValue] = useState("");
 
   return (
-    <div className="box">
-      {output.map((section, index) => {
-        return (
-          <div key={index}>
-            <h1>{section.title}</h1>
-            {section.elements.map((element, index) => (
-              <p className="element" key={index}>
-                {element}
-              </p>
-            ))}
-          </div>
-        );
-      })}
+    <div>
+      <input type="text" value={inpValue} onChange={(e)=>{setInpValue(e.target.value)}}/>
+      <button onClick={()=>{setInpValue("")}}>clear</button>
+      <h1>{inpValue}</h1>
     </div>
   );
 }
