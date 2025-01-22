cd039fbdd0d19f7c3b05363aa561cdaa8173ace4
LivinkumarS <001liv100@ggmail.com>
2025-01-22 13:46:08 +0530

    ToDo List Mini Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,58 @@
 import React, { useState } from "react";
+import "./App.css";
 
 export default function App() {
-  const [inpValue, setInpValue] = useState("");
+  const [inpValue, setInputValue] = useState("");
+
+  const [tasks, setTasks] = useState([]);
+
+  function addTask() {
+    setTasks((prev) => {
+      return [...prev, inpValue];
+    });
+
+    setInputValue("");
+  }
+
+  function clearTask(index) {
+    const updatedTask = tasks.filter((element, ind) => {
+      return ind !== index;
+    });
+    setTasks(updatedTask);
+  }
 
   return (
-    <div>
-      <input type="text" value={inpValue} onChange={(e)=>{setInpValue(e.target.value)}}/>
-      <button onClick={()=>{setInpValue("")}}>clear</button>
-      <h1>{inpValue}</h1>
+    <div className="box">
+      <h1 className="title">ToDo List</h1>
+      <input
+        type="text"
+        value={inpValue}
+        onChange={(e) => {
+          setInputValue(e.target.value);
+        }}
+      />
+      <button onClick={addTask}>Submit</button>
+
+      <div className="tasks">
+        {tasks.map((element, index) => {
+          return (
+            <div className="task" key={index}>
+              <h4>
+                {index + 1}
+                {") "}
+                {element}
+              </h4>{" "}
+              <button
+                onClick={() => {
+                  clearTask(index);
+                }}
+              >
+                clear
+              </button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
